6908c53d1693d746d9a86d3d7db095db663e7798
AnsonSGilbert <ansonsgilbert@gmail.com>
2024-10-17 22:07:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+document.getElementById("Download").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = "path/to/your/resume.pdf"; // Replace with the actual path to your PDF
+    link.download = "Resume.pdf"; // Optional: specify the download file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
